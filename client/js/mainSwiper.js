9ea3a133fe14ffaa3cab2abc454a8fa07df863b8
ParkjiDev <parkjidev@gmail.com>
2023-12-25 19:28:07 +0900

    [add] swiper slide 사진에 따라 선수 정보 변경되도록 설정

--- client/js/mainSwiper.js
+++ client/js/mainSwiper.js
@@ -1,15 +1,15 @@
-console.log(data);
+import { getNode } from "../lib/index.js";
+
 const swiper = new Swiper(".swiper", {
-  loop: true, // loop
-  speed: 1000, // added(slide speed)
+  loop: true,
+  speed: 1000,
   effect: "fade",
   fadeEffect: {
-    // added
-    crossFade: true, // added(resolve the overlapping of the slides)
+    crossFade: true,
   },
   pagination: {
-    // pagination(dots)
     el: ".swiper-pagination",
+    clickable: true,
     renderBullet: function (index, className) {
       return /* html */ `
         <span class="${className}">
@@ -19,8 +19,32 @@ const swiper = new Swiper(".swiper", {
     },
   },
   navigation: {
-    // navigation(arrows)
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  on: {
+    slideChangeTransitionStart: function () {
+      const slide = getNode(".swiper-slide-active");
+      changeCenterText(slide);
+    },
+  },
 });
+
+function changeCenterText(slide) {
+  const index = slide.dataset.index - 1;
+  const playerData = data[index];
+  changeText(".name", playerData.name);
+  changeText(".name-ko", playerData.nameKo);
+  changeText(".speaking i", playerData.speaking);
+  changeText(".speaking-source", playerData.speakingSource);
+  changeText(".profile-birthday", playerData.birthday);
+  changeText(".profile-team", playerData.team);
+  changeText(".profile-lane", playerData.lane);
+  changeText(".profile-debut", playerData.debut);
+}
+
+function changeText(node, text) {
+  const textNode = getNode(node);
+  textNode.textContent = text;
+}
